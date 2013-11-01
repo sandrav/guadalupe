bd54b58639428eb2920a2a06b2c132a370d3b203
Sandra <sandra@sandra-ThinkPad-E420.(none)>
2013-11-01 15:39:49 -0300

    actualizacion temas y agrego plugin para sacar qtranslate en forma limpia

---
+++ db_automation/prod_to_ local_db_automation.sql
@@ -0,0 +1,4 @@
+UPDATE mariaguadalupe.wp_options SET option_value = "http://localhost/guadalupe" WHERE (option_name="home" OR option_name="siteurl") AND option_value="http://www.mariaguadalupe.org.ar" ;
+
+UPDATE mariaguadalupe.wp_options SET option_value = "http://localhost/guadalupe" WHERE (option_name="home" OR option_name="siteurl") AND option_value="http://www.mariaguadalupe.org.ar/" ;
+
